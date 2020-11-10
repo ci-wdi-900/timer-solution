8442f57a0ea8b1945ca14c329ff69d597d4b01bc
Colin Jaffe <balloonasaurus@gmail.com>
2020-11-10 11:33:04 -0500

    Updated as coded in class.

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ console.log('\u0007');
 // version 1
 // setTimeout(function() {
 //     console.log('\u0007');
+//     console.log('3 seconds has passed!')
 // }, 3000);
 
 // version 2
 // const time = process.argv[2];
+
 // setTimeout(function() {
 //     console.log(time + ' seconds have passed!');
 //     console.log('\u0007');
@@ -16,9 +18,10 @@ console.log('\u0007');
 // version 3
 // const times = process.argv.slice(2);
 // for (const time of times) {
-//     setTimeout(function() {
-//         console.log('\u0007');
-//     }, time * 1000)
+//   setTimeout(function () {
+//     console.log('\u0007');
+//     console.log(`Time's up!`)
+//   }, time * 1000)
 // }
 
 // version 4
@@ -26,20 +29,23 @@ console.log('\u0007');
 // for (const time of times) {
 //     setTimeout(function() {
 //         console.log('\u0007');
-//         console.log(time + ' seconds have passed!');
+    //     console.log(`This is your reminder that ${ time } seconds have passed.`);
 //     }, time * 1000)
 // }
 
 // version 5
 // const times = process.argv.slice(2);
+// const seconds = 1000;
+// const minutes = 60;
+// const hours = 60;
 // for (const time of times) {
 //     let ms = time;
 //     if (ms.endsWith('m')) {
-//         ms = ms.slice(0, -1) * 60000;
+//         ms = ms.slice(0, -1) * seconds * minutes;
 //     } else if (ms.endsWith('h')) {
-//         ms = ms.slice(0, -1) * 3600000;
+//         ms = ms.slice(0, -1) * seconds * minutes * hours;
 //     } else {
-//         ms = time * 1000
+//         ms = time * seconds;
 //     }
 
 //     setTimeout(function() {
