544f723c3e79cfa54addc4c927756d82c8eafb42
Colin Jaffe <balloonasaurus@gmail.com>
2020-11-09 21:00:38 -0500

    Adds solution through latest stretch goal.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Timer - Solution
+
+One possible set of solutions to the [Timer](https://github.com/ci-wdi-900/timer) project.
--- main.js
+++ main.js
@@ -3,20 +3,21 @@ console.log('\u0007');
 
 // version 1
 // setTimeout(function() {
-//     process.stdout.write('\u0007');
+//     console.log('\u0007');
 // }, 3000);
 
 // version 2
 // const time = process.argv[2];
 // setTimeout(function() {
-//     process.stdout.write('\u0007');
+//     console.log(time + ' seconds have passed!');
+//     console.log('\u0007');
 // }, time * 1000);
 
 // version 3
 // const times = process.argv.slice(2);
 // for (const time of times) {
 //     setTimeout(function() {
-//         process.stdout.write('\u0007');
+//         console.log('\u0007');
 //     }, time * 1000)
 // }
 
@@ -24,25 +25,45 @@ console.log('\u0007');
 // const times = process.argv.slice(2);
 // for (const time of times) {
 //     setTimeout(function() {
-//         process.stdout.write('\u0007');
+//         console.log('\u0007');
 //         console.log(time + ' seconds have passed!');
 //     }, time * 1000)
 // }
 
 // version 5
-const times = process.argv.slice(2);
-for (const time of times) {
-    let ms = time;
-    if (ms.endsWith('m')) {
-        ms = ms.slice(0, -1) * 60000;
-    } else if (ms.endsWith('h')) {
-        ms = ms.slice(0, -1) * 3600000;
-    } else {
-        ms = time * 1000
-    }
+// const times = process.argv.slice(2);
+// for (const time of times) {
+//     let ms = time;
+//     if (ms.endsWith('m')) {
+//         ms = ms.slice(0, -1) * 60000;
+//     } else if (ms.endsWith('h')) {
+//         ms = ms.slice(0, -1) * 3600000;
+//     } else {
+//         ms = time * 1000
+//     }
 
-    setTimeout(function() {
-        process.stdout.write('\u0007');
-        console.log(time + ' has passed!');
-    }, ms)
-}
+//     setTimeout(function() {
+//         console.log('\u0007');
+//         console.log(time + ' has passed!');
+//     }, ms)
+// }
+
+// version 6
+// const time = process.argv[2];
+// const message = process.argv.slice(3).join(' ');
+// let ms = time;
+// if (ms.endsWith('m')) {
+//     ms = ms.slice(0, -1) * 60000;
+// } else if (ms.endsWith('h')) {
+//     ms = ms.slice(0, -1) * 3600000;
+// } else {
+//     ms = time * 1000
+// }
+
+// setTimeout(function() {
+//     console.log('\u0007');
+//     console.log(message);
+// }, ms)
+
+// version 7
+// TODO
